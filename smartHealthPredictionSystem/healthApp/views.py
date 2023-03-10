3d0cc0c34902d9b849283632e9fee6c1ae67de0f
Anmol Vishvas <anmol23vishvas@gmail.com>
2023-03-10 13:56:54 +0400

    view prediction table added

--- smartHealthPredictionSystem/healthApp/views.py
+++ smartHealthPredictionSystem/healthApp/views.py
@@ -201,8 +201,15 @@ def delete_patient(request, pid):
 
 @login_required(login_url="login")
 def AdminViewPredictionResultsPage(request):
-    return render(request, 'Admin_ViewPredictionResult.html')
+    prediction_data = PredictionData.objects.all()
+    prediction_data_dict = {'prediction_data': prediction_data}
+    return render(request, 'Admin_ViewPredictionResult.html',prediction_data_dict)
 
+@login_required(login_url="login")
+def delete_prediction_admin(request, pid):
+    pred = PredictionData.objects.get(id=pid)
+    pred.delete()
+    return redirect('admin_view_prediction')
 
 @login_required(login_url="login")
 def AdminViewFeedbackPage(request):
@@ -302,22 +309,30 @@ def PatientFeedbackPage(request):
     d = {'message': error, 'userr': sign}
     return render(request, 'Patient_Feedback.html', d)
 
-
+@login_required(login_url="login")
 def PatientPredictionHistoryPage(request):
-    return render(request, 'Patient_PredictionHistory.html')
+    prediction_data = PredictionData.objects.all()
+    prediction_data_dict = {'prediction_data': prediction_data}
+    return render(request, 'Patient_PredictionHistory.html', prediction_data_dict)
+
+@login_required(login_url="login")
+def delete_prediction(request, pid):
+    pred = PredictionData.objects.get(id=pid)
+    pred.delete()
+    return redirect('patient_prediction_history')
 
 @login_required(login_url="login")
 def PatientHealthPredictionPage(request):
     predictionDetails = None
     listDiseases = []
 
-    if request.method=="POST":
-        for i,j in request.POST.items():
+    if request.method == "POST":
+        for i, j in request.POST.items():
             if "csrfmiddlewaretoken" != i:
                 listDiseases.append(i)
         # training.csv
         DATA_PATH = Admin_Health_CSV.objects.get(id=1)
-        data = pd.read_csv(DATA_PATH.csv_file).dropna(axis = 1)
+        data = pd.read_csv(DATA_PATH.csv_file).dropna(axis=1)
 
         # Checking whether the dataset is balanced or not
         disease_counts = data["prognosis"].value_counts()
@@ -326,8 +341,8 @@ def PatientHealthPredictionPage(request):
             "Counts": disease_counts.values
         })
 
-        plt.figure(figsize = (18,8))
-        sns.barplot(x = "Disease", y = "Counts", data = temp_df)
+        plt.figure(figsize=(18, 8))
+        sns.barplot(x="Disease", y="Counts", data=temp_df)
         plt.xticks(rotation=90)
 
         # Encoding the target value into numerical
@@ -335,20 +350,20 @@ def PatientHealthPredictionPage(request):
         encoder = LabelEncoder()
         data["prognosis"] = encoder.fit_transform(data["prognosis"])
 
-        X = data.iloc[:,:-1]
+        X = data.iloc[:, :-1]
         y = data.iloc[:, -1]
-        X_train, X_test, y_train, y_test =train_test_split(
-        X, y, test_size = 0.2, random_state = 24)
+        X_train, X_test, y_train, y_test = train_test_split(
+            X, y, test_size=0.2, random_state=24)
 
         symptoms = X.columns.values
         symptom_index = {}
         for index, value in enumerate(symptoms):
             symptom = " ".join([i.capitalize() for i in value.split("_")])
             symptom_index[symptom] = index
-        
+
         data_dict = {
-            "symptom_index":symptom_index,
-            "predictions_classes":encoder.classes_
+            "symptom_index": symptom_index,
+            "predictions_classes": encoder.classes_
         }
 
         final_svm_model = SVC()
@@ -358,7 +373,7 @@ def PatientHealthPredictionPage(request):
         final_nb_model.fit(X, y)
         final_rf_model.fit(X, y)
 
-        #Testing.csv
+        # Testing.csv
         DATA_PATH2 = Admin_Health_CSV.objects.get(id=2)
         test_data = pd.read_csv(DATA_PATH2.csv_file).dropna(axis=1)
 
@@ -369,52 +384,58 @@ def PatientHealthPredictionPage(request):
         nb_preds = final_nb_model.predict(test_X)
         rf_preds = final_rf_model.predict(test_X)
 
-        final_preds = [mode([i,j,k])[0][0] for i,j,
-                    k in zip(svm_preds, nb_preds, rf_preds)]
+        final_preds = [mode([i, j, k])[0][0] for i, j,
+                       k in zip(svm_preds, nb_preds, rf_preds)]
 
         print(f"Accuracy on Test dataset by the combined model\
         : {accuracy_score(test_Y, final_preds)*100}")
 
         cf_matrix = confusion_matrix(test_Y, final_preds)
-        plt.figure(figsize=(12,8))
+        plt.figure(figsize=(12, 8))
 
-        sns.heatmap(cf_matrix, annot = True)
+        sns.heatmap(cf_matrix, annot=True)
 
         def predictDisease(symptoms):
-            
+
             # # creating input data for the models
             input_data = [0] * len(data_dict["symptom_index"])
             for symptom in symptoms:
                 index = data_dict["symptom_index"][symptom]
                 input_data[index] = 1
-                
+
             # reshaping the input data and converting it into suitable format for model predictions
-            input_data = np.array(input_data).reshape(1,-1)
-            
+            input_data = np.array(input_data).reshape(1, -1)
+
             # generating individual outputs
-            rf_prediction = data_dict["predictions_classes"][final_rf_model.predict(input_data)[0]]
-            nb_prediction = data_dict["predictions_classes"][final_nb_model.predict(input_data)[0]]
-            svm_prediction = data_dict["predictions_classes"][final_svm_model.predict(input_data)[0]]
-            
+            rf_prediction = data_dict["predictions_classes"][final_rf_model.predict(input_data)[
+                0]]
+            nb_prediction = data_dict["predictions_classes"][final_nb_model.predict(input_data)[
+                0]]
+            svm_prediction = data_dict["predictions_classes"][final_svm_model.predict(input_data)[
+                0]]
+
             # making final prediction by taking mode of all predictions
-            final_prediction = mode([rf_prediction, nb_prediction, svm_prediction])[0][0]
+            final_prediction = mode(
+                [rf_prediction, nb_prediction, svm_prediction])[0][0]
             predictions = {
                 "RandomForestClassifier Prediction": rf_prediction,
                 "GaussianNB Prediction": nb_prediction,
                 "SVC Prediction": svm_prediction,
-                "Final Prediction":final_prediction
+                "Final Prediction": final_prediction
             }
             return predictions
 
         # Testing the function
         predictionDetails = predictDisease(listDiseases)
         patient = Patient.objects.get(user=request.user)
-        PredictionData.objects.create(patient=patient, prediction_accuracy=round(accuracy_score(test_Y, final_preds)*100,2), result=predictionDetails["Final Prediction"], values_list=listDiseases, predict_for="General Health Prediction")
+        PredictionData.objects.create(patient=patient, prediction_accuracy=round(accuracy_score(test_Y, final_preds)*100, 2),
+                                      result=predictionDetails["Final Prediction"], values_list=listDiseases, predict_for="General Health Prediction")
         # print(listDiseases)
-    alldisease = ['Itching','Skin Rash','Nodal Skin Eruptions','Continuous Sneezing','Shivering','Chills','Joint Pain',	'Stomach Pain','Acidity','Ulcers On Tongue','Muscle Wasting','Vomiting','Burning Micturition','Fatigue','Weight Gain','Anxiety','Cold Hands And Feets','Mood Swings','Weight Loss','Restlessness','Lethargy','Patches In Throat','Irregular Sugar Level','Cough','High Fever','Sunken Eyes','Breathlessness','Sweating','Dehydration',	'Indigestion','Headache','Yellowish Skin','Dark Urine','Nausea','Loss Of Appetite','Pain Behind The Eyes','Back Pain','Constipation','Abdominal Pain','Diarrhoea','Mild Fever','Yellow Urine','Yellowing Of Eyes','Acute Liver Failure','Fluid Overload','Swelling Of Stomach','Swelled Lymph Nodes','Malaise','Blurred And Distorted Vision','Phlegm','Throat Irritation','Redness Of Eyes','Sinus Pressure','Runny Nose','Congestion','Chest Pain','Weakness In Limbs','Fast Heart Rate',	'Pain During Bowel Movements','Pain In Anal Region','Bloody Stool','Irritation In Anus','Neck Pain','Dizziness','Cramps','Bruising','Obesity','Swollen Legs','Swollen Blood Vessels','Puffy Face And Eyes','Enlarged Thyroid','Brittle Nails','Swollen Extremeties','Excessive Hunger','Extra Marital Contacts','Drying And Tingling Lips','Slurred Speech','Knee Pain','Hip Joint Pain','Muscle Weakness','Stiff Neck','Swelling Joints','Movement Stiffness','Spinning Movements','Loss Of Balance','Unsteadiness','Weakness Of One Body Side','Loss Of Smell','Bladder Discomfort','Continuous Feel Of Urine','Passage Of Gases','Internal Itching','Toxic Look (Typhos)',	'Depression','Irritability','Muscle Pain','Altered Sensorium','Red Spots Over Body','Belly Pain','Abnormal Menstruation','Dischromic Patches','Watering From Eyes','Increased Appetite','Polyuria','Family History','Mucoid Sputum','Rusty Sputum','Lack Of Concentration',	'Visual Disturbances','Receiving Blood Transfusion','Receiving Unsterile Injections','Coma','Stomach Bleeding',	'Distention Of Abdomen','History Of Alcohol Consumption','Fluid Overload','Blood In Sputum','Prominent Veins On Calf','Palpitations','Painful Walking','Pus Filled Pimples', 'Blackheads','Scurring','Skin Peeling','Silver Like Dusting','Small Dents In Nails','Inflammatory Nails','Blister','Red Sore Around Nose','Yellow Crust Ooze','Prognosis']
-    return render(request,'Patient_HealthPrediction.html', {'alldisease':alldisease, 'predictionDetails':predictionDetails})
+    alldisease = ['Itching', 'Skin Rash', 'Nodal Skin Eruptions', 'Continuous Sneezing', 'Shivering', 'Chills', 'Joint Pain',	'Stomach Pain', 'Acidity', 'Ulcers On Tongue', 'Muscle Wasting', 'Vomiting', 'Burning Micturition', 'Fatigue', 'Weight Gain', 'Anxiety', 'Cold Hands And Feets', 'Mood Swings', 'Weight Loss', 'Restlessness', 'Lethargy', 'Patches In Throat', 'Irregular Sugar Level', 'Cough', 'High Fever', 'Sunken Eyes', 'Breathlessness', 'Sweating', 'Dehydration',	'Indigestion', 'Headache', 'Yellowish Skin', 'Dark Urine', 'Nausea', 'Loss Of Appetite', 'Pain Behind The Eyes', 'Back Pain', 'Constipation', 'Abdominal Pain', 'Diarrhoea', 'Mild Fever', 'Yellow Urine', 'Yellowing Of Eyes', 'Acute Liver Failure', 'Fluid Overload', 'Swelling Of Stomach', 'Swelled Lymph Nodes', 'Malaise', 'Blurred And Distorted Vision', 'Phlegm', 'Throat Irritation', 'Redness Of Eyes', 'Sinus Pressure', 'Runny Nose', 'Congestion', 'Chest Pain', 'Weakness In Limbs', 'Fast Heart Rate',	'Pain During Bowel Movements', 'Pain In Anal Region', 'Bloody Stool', 'Irritation In Anus', 'Neck Pain', 'Dizziness', 'Cramps', 'Bruising', 'Obesity', 'Swollen Legs', 'Swollen Blood Vessels', 'Puffy Face And Eyes', 'Enlarged Thyroid',
+                  'Brittle Nails', 'Swollen Extremeties', 'Excessive Hunger', 'Extra Marital Contacts', 'Drying And Tingling Lips', 'Slurred Speech', 'Knee Pain', 'Hip Joint Pain', 'Muscle Weakness', 'Stiff Neck', 'Swelling Joints', 'Movement Stiffness', 'Spinning Movements', 'Loss Of Balance', 'Unsteadiness', 'Weakness Of One Body Side', 'Loss Of Smell', 'Bladder Discomfort', 'Continuous Feel Of Urine', 'Passage Of Gases', 'Internal Itching', 'Toxic Look (Typhos)',	'Depression', 'Irritability', 'Muscle Pain', 'Altered Sensorium', 'Red Spots Over Body', 'Belly Pain', 'Abnormal Menstruation', 'Dischromic Patches', 'Watering From Eyes', 'Increased Appetite', 'Polyuria', 'Family History', 'Mucoid Sputum', 'Rusty Sputum', 'Lack Of Concentration',	'Visual Disturbances', 'Receiving Blood Transfusion', 'Receiving Unsterile Injections', 'Coma', 'Stomach Bleeding',	'Distention Of Abdomen', 'History Of Alcohol Consumption', 'Fluid Overload', 'Blood In Sputum', 'Prominent Veins On Calf', 'Palpitations', 'Painful Walking', 'Pus Filled Pimples', 'Blackheads', 'Scurring', 'Skin Peeling', 'Silver Like Dusting', 'Small Dents In Nails', 'Inflammatory Nails', 'Blister', 'Red Sore Around Nose', 'Yellow Crust Ooze', 'Prognosis']
+    return render(request, 'Patient_HealthPrediction.html', {'alldisease': alldisease, 'predictionDetails': predictionDetails})
 
-    
+
 @login_required(login_url="login")
 def PatientSettingsPage(request):
     sign = 0
@@ -515,10 +536,17 @@ def DoctorEditProfilePage(request):
     d = {'message': message, 'userr': sign}
     return render(request, 'Doctor_EditProfile.html', d)
 
-
+@login_required(login_url="login")
 def DoctorViewPredictionResultsPage(request):
-    return render(request, 'Doctor_PredictionResult.html')
+    prediction_data = PredictionData.objects.all()
+    prediction_data_dict = {'prediction_data': prediction_data}
+    return render(request, 'Doctor_PredictionResult.html', prediction_data_dict)
 
+@login_required(login_url="login")
+def delete_prediction_doc(request, pid):
+    pred = PredictionData.objects.get(id=pid)
+    pred.delete()
+    return redirect('doctor_view_prediction_result')
 
 @login_required(login_url="login")
 def DoctorSettingsPage(request):
